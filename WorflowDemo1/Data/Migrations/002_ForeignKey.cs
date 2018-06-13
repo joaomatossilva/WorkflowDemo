072b28b45245062d79e7b1bd49058f1099f1dba2
Joao Matos Silva <kappy@acydburne.com.pt>
2018-06-13 09:39:43 +0100

    Foreign key as a normal migration

--- WorflowDemo1/Data/Migrations/002_ForeignKey.cs
+++ WorflowDemo1/Data/Migrations/002_ForeignKey.cs
@@ -8,13 +8,18 @@ using FluentMigrator;
 namespace WorflowDemo1.Data.Migrations
 {
     [Migration(201812062250)]
-    public class _002_ForeignKey : AutoReversingMigration
+    public class _002_ForeignKey : Migration
     {
         public override void Up()
         {
             Create.ForeignKey("FK_ProductCategory")
                 .FromTable("Products").ForeignColumn("CategoryId").ToTable("Categories").PrimaryColumn("Id")
                     .OnDelete(Rule.Cascade);
+        }
+
+        public override void Down()
+        {
+            Delete.ForeignKey("FK_ProductCategory").OnTable("Products");
         }
     }
 }
